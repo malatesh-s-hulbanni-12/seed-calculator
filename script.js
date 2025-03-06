d648c470b0d9ad28cd8934e63a4c4a7cd528e2f9
malatesh s hulbanni <mshulbanni95@gmail.com>
2025-03-06 13:57:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleCommissionInput() {
+    const commission = document.getElementById("commission").value;
+    const commissionInput = document.getElementById("commissionInput");
+    commissionInput.style.display = commission === "1" ? "block" : "none";
+}
+
+function calculateTotal() {
+    const packets = parseInt(document.getElementById("packets").value) || 0;
+    const kgPerPacket = parseInt(document.getElementById("kgPerPacket").value) || 0;
+    const amountPerQuintal = parseInt(document.getElementById("amountPerQuintal").value) || 0;
+    const dalliAmali = parseInt(document.getElementById("dalliAmali").value) || 0;
+    const commission = parseInt(document.getElementById("commission").value) || 0;
+    const commissionPercent = parseInt(document.getElementById("commissionPercent").value) || 0;
+  
+    const totalKg = packets * kgPerPacket;
+    const amountPerKg = amountPerQuintal / 100;
+    const seedAmount = totalKg * amountPerKg;
+    const dalliAmaliTotal = packets * dalliAmali;
+    let totalAmount = seedAmount - dalliAmaliTotal;
+    const commissionAmount = (seedAmount * commissionPercent) /100;
+  
+    if (commission === 1) {
+      const commissionAmount = (seedAmount * commissionPercent) / 100;
+      totalAmount -= commissionAmount;
+    }
+  
+    const output = document.getElementById("output");
+    output.innerHTML = `
+      <p>Total Quintal of seed: <strong>${totalKg / 100} Quintal</strong></p>
+      <p>Total Amount of seed(with commision): <strong>${seedAmount.toFixed(2)}</strong></p>
+      <p>amount per quintal : <strong>${amountPerQuintal}</strong></p>
+      <p>dalali and amali Amount : <strong>${dalliAmaliTotal}</strong></p>
+      <p>commission Amount : <strong>${commissionAmount}</strong></p>
+      <p>Total Amount : <strong>${totalAmount.toFixed(2)}</strong></p>
+    `;
+  }
+  
