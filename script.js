6041e95a9b15a24f6d9c6c0ec4144dd9ff869ea5
malatesh s hulbanni <mshulbanni95@gmail.com>
2025-11-20 17:54:18 +0530

    Updated code with animation and style

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ function toggleCommissionInput() {
 
 function calculateTotal() {
     const packets = parseInt(document.getElementById("packets").value) || 0;
+    const rpacket = parseInt(document.getElementById("rpacket").value) || 0;
     const kgPerPacket = parseInt(document.getElementById("kgPerPacket").value) || 0;
     const amountPerQuintal = parseInt(document.getElementById("amountPerQuintal").value) || 0;
     const dalliAmali = parseInt(document.getElementById("dalliAmali").value) || 0;
+    const Amali = parseInt(document.getElementById("Amali").value) || 0;
     const commission = parseInt(document.getElementById("commission").value) || 0;
     const commissionPercent = parseInt(document.getElementById("commissionPercent").value) || 0;
   
-    const totalKg = packets * kgPerPacket;
+    const totalKg = (packets * kgPerPacket)+rpacket;
     const amountPerKg = amountPerQuintal / 100;
     const seedAmount = totalKg * amountPerKg;
     const dalliAmaliTotal = packets * dalliAmali;
-    let totalAmount = seedAmount - dalliAmaliTotal;
+    const AmaliTotal = packets * Amali;
+    let totalAmount = seedAmount - (dalliAmaliTotal+AmaliTotal);
     const commissionAmount = (seedAmount * commissionPercent) /100;
   
     if (commission === 1) {
@@ -30,8 +33,9 @@ function calculateTotal() {
       <p>Total Amount of seed(with commision): <strong>${seedAmount.toFixed(2)}</strong></p>
       <p>amount per quintal : <strong>${amountPerQuintal}</strong></p>
       <p>dalali and amali Amount : <strong>${dalliAmaliTotal}</strong></p>
+      <p>amali Amount : <strong>${AmaliTotal}</strong></p>
       <p>commission Amount : <strong>${commissionAmount}</strong></p>
-      <p>Total Amount : <strong>${totalAmount.toFixed(2)}</strong></p>
+      <p class="highlight">Total Amount : <strong>${totalAmount.toFixed(2)}</strong></p>
     `;
   }
   
