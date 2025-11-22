2dc89a2036effa4f7500e9f71cdb26885dbc1664
malatesh s hulbanni <mshulbanni95@gmail.com>
2025-11-22 18:30:16 +0530

    update files

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function calculateTotal() {
     const rpacket = parseInt(document.getElementById("rpacket").value) || 0;
     const kgPerPacket = parseInt(document.getElementById("kgPerPacket").value) || 0;
     const amountPerQuintal = parseInt(document.getElementById("amountPerQuintal").value) || 0;
+    /* 
     const dalliAmali = parseInt(document.getElementById("dalliAmali").value) || 0;
+    */
     const Amali = parseInt(document.getElementById("Amali").value) || 0;
     const commission = parseInt(document.getElementById("commission").value) || 0;
     const commissionPercent = parseInt(document.getElementById("commissionPercent").value) || 0;
@@ -17,9 +19,11 @@ function calculateTotal() {
     const totalKg = (packets * kgPerPacket)+rpacket;
     const amountPerKg = amountPerQuintal / 100;
     const seedAmount = totalKg * amountPerKg;
+    /*
     const dalliAmaliTotal = packets * dalliAmali;
+    */
     const AmaliTotal = packets * Amali;
-    let totalAmount = seedAmount - (dalliAmaliTotal+AmaliTotal);
+    let totalAmount = seedAmount - (/*dalliAmaliTotal*/+AmaliTotal);
     const commissionAmount = (seedAmount * commissionPercent) /100;
   
     if (commission === 1) {
@@ -29,10 +33,9 @@ function calculateTotal() {
   
     const output = document.getElementById("output");
     output.innerHTML = `
-      <p>Total Quintal of seed: <strong>${totalKg / 100} Quintal</strong></p>
+      <p>Total Quintal of seed: <strong class="highlight1">${totalKg / 100} Quintal</strong></p>
       <p>Total Amount of seed(with commision): <strong>${seedAmount.toFixed(2)}</strong></p>
       <p>amount per quintal : <strong>${amountPerQuintal}</strong></p>
-      <p>dalali and amali Amount : <strong>${dalliAmaliTotal}</strong></p>
       <p>amali Amount : <strong>${AmaliTotal}</strong></p>
       <p>commission Amount : <strong>${commissionAmount}</strong></p>
       <p class="highlight">Total Amount : <strong>${totalAmount.toFixed(2)}</strong></p>
